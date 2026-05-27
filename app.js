5116bacdec20080a72c938b3a751f4f06faf3558
Piyush <normipiyush69@gmail.com>
2026-05-27 11:00:56 +0530

    updated code
    added better comments for better understanding

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require("express");
 const data = require("./data.json")
 const app = express();
 
+// require path
+const path = require("path")
+
 // ejs basic code-
 app.set('view engine', 'ejs');
+
+// ye express ko batayega ki hamesaha views wala folder yahi milega!
+app.set("views", path.join(__dirname, "/views"))
+
+// serving static files
+app.use(express.static(path.join(__dirname, "/public")))
 
 
 app.listen(3000, ()=> {
@@ -22,5 +31,10 @@ app.get("/about/:username", (req, res)=>{
     const { username } = req.params;
     let user_arr = data.users
     let foundUser = user_arr.find(user => user.name.toLowerCase() === username.toLowerCase())
-    res.render('user.ejs', {foundUser})
+    // added logic to check if user exists or not before rendering page!
+    if(foundUser){
+        res.render('user.ejs', {foundUser})
+    } else {
+        res.send("NO DATA FOUND")
+    }
 })
